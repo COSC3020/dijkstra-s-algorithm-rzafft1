e6e156a59a86250b18ff6b01d0ad2159bf3fd453
Ryan Zafft <123418615+rzafft1@users.noreply.github.com>
2023-10-18 22:03:55 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,76 @@
-function dijkstra(graph, sourceNode) {
-    return [];
+// search through the distances and find the min distance 
+// return the unmarked node with the min distance
+// initialze the min distance to infinity and the unmarked node to infinity
+// search through row 
+// if the current node has not been visited
+    // update the min distance to the currentnodes distance
+    // set the current node as unmarked
+// return the currentnode with the smallest min distance
+function getUnmarkedNode(dist, visited, n)
+{
+    let minDist = Infinity
+    let unmarked = Infinity
+    for (let v = 0; v < n; v++)
+    {
+        if (visited[v] == false && dist[v] < minDist)
+        {
+            minDist = dist[v]
+            unmarked = v
+        }
+    }
+    return unmarked
 }
+
+// initalize sourceNode distance to zero
+// initalize first unmarked node to source
+// while there are unmarked vertices left 
+    // get the unmarked distance/node 
+    // check if unmarked is infinity
+    // set unmarked to marked 
+    // find min distance for every node 
+// return distances 
+function _dijkstra(graph, sourceNode, visited, dist) 
+{
+    dist[sourceNode] = 0;
+    let unmarked = sourceNode;
+    unmarkedNodes = true
+
+    while (unmarkedNodes) 
+    {
+        unmarked = getUnmarkedNode(dist, visited, graph.length);
+
+        if (unmarked != Infinity) 
+        {
+            visited[unmarked] = true;
+                
+            for (let node = 0; node < graph.length; node++) 
+            {
+                if (!visited[node] && graph[unmarked][node] > 0) 
+                {
+                    dist[node] = Math.min(dist[node], dist[unmarked] + graph[unmarked][node]);
+                }
+            }
+        }
+        else
+        {
+            unmarkedNodes = false
+        }
+        
+    }
+    return dist;
+}
+
+// initalize distances to infinity 
+// intialize all visited entries to false
+// call algorithm
+function dijkstra(graph, sourceNode) 
+{
+    var dist = Array(graph.length).fill(Infinity) 
+    let visited = Array(graph.length).fill(false) 
+
+    let shortestPaths = _dijkstra(graph, sourceNode, visited, dist)
+    
+    //console.log(shortestPaths)
+    return shortestPaths
+}
+
